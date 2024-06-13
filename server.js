19c9bdf74254e9aafbb1fcc829a121fc8d94792a
unknown <bermudezbastidass@uvm.edu.ve>
2024-06-13 09:18:15 -0400

    Add data bd

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const SECRET_KEY = 'your_secret_key';
+const SECRET_KEY = process.env.SECRET_KEY || 'your_secret_key';
 
 // Configuración de la base de datos
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'salva',
-  database: 'gym'
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME
 });
 
 db.connect((err) => {
