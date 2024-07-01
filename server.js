c3f84dbdc32966cf3e2b41a75e6309797904d09a
unknown <bermudezbastidass@uvm.edu.ve>
2024-07-01 08:34:58 -0400

    Update backend settings

--- server.js
+++ server.js
@@ -99,10 +99,10 @@ app.delete('/api/clientes/:id', verifyToken, (req, res) => {
 });
 
 // Sirve el frontend de React
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '..', 'build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
